83cb59301c16bc2d62cc7748da36bddbdda275f7
Sergej <gorbatschewsergej@gmail.com>
2023-12-26 22:14:31 +0900

    Add functionality to operator buttons Added the functions for the operations.

--- javascript.js
+++ javascript.js
@@ -6,28 +6,37 @@ const multiplyOp = document.getElementById("multiply");
 const devideOp = document.getElementById("devide");
 const equal = document.getElementById("equal");
 
-clear.addEventListener("click", () => screen.innerHTML = "");
+clear.addEventListener("click", function() {
+    screen.innerHTML = "";
+    op = "";
+    num1 = 0;
+    num2 = 0;
+});
 
 const btns = document.querySelectorAll(".number");
 
 for (const btn of btns) {
-    btn.addEventListener("click", function() {screen.innerHTML += this.value})
+    btn.addEventListener("click", function() {screen.innerHTML += this.value});
 };
 
 function add(a, b) {
-    return a + b;
+    let result = a + b
+    return result;
 };
 
 function subtract(a, b) {
-    return a - b;
+    let result = a - b
+    return result;
 };
 
 function multiply(a, b) {
-    return a * b;
+    let result = a * b
+    return result;
 };
 
 function devide(a, b) {
-    return a / b;
+    let result = a / b
+    return result;
 };
 
 let num1;
@@ -35,8 +44,48 @@ let op;
 let num2;
 
 function operate(a, op, b) {
-    a = num1;
-    op = op;
-    b = num2;
-    
-}
+    if (op == "+") {
+       return add(a, b)
+    } else if (op == "-") {
+        return subtract(a, b)
+    } else if (op == "*") {
+        return multiply(a, b)
+    } else if (op == "/") {
+        return devide(a, b)
+    };
+};
+
+plus.addEventListener("click", function() {
+    op = "+";
+    num1 = +screen.innerHTML;
+    screen.innerHTML = "";
+    console.log(num1);
+});
+
+minus.addEventListener("click", function() {
+    op = "-";
+    num1 = +screen.innerHTML;
+    screen.innerHTML = "";
+    console.log(num1);
+});
+
+multiplyOp.addEventListener("click", function() {
+    op = "*";
+    num1 = +screen.innerHTML;
+    screen.innerHTML = "";
+    console.log(num1);
+});
+
+devideOp.addEventListener("click", function() {
+    op = "/";
+    num1 = +screen.innerHTML;
+    screen.innerHTML = "";
+    console.log(num1);
+});
+
+equal.addEventListener("click", function() {
+    num2 = +screen.innerHTML;
+    console.log(num2);
+    let result = operate(num1, op, num2);
+    screen.innerHTML = result;
+})
